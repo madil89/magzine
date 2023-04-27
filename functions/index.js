4842364caa67c88e3737eb8cf58f4b60528a10cd
Muhammad Adil <madil89@gmail.com>
2023-04-28 00:14:04 +0500

    added app check

--- functions/index.js
+++ functions/index.js
@@ -11,26 +11,42 @@ const listUsers = () => {
     return (userList);
   });
 };
-exports.getUsers = functions.https.onCall((data, context) => {
-  if (context.auth.token.admin) {
-    return listUsers();
-  } else {
-    throw new functions.https.HttpsError("permission-denied",
-        "You don't have admin permission");
-  }
-});
+exports.getUsers = functions.runWith({enforceAppCheck: true})
+    .https.onCall((data, context) => {
+      if (context.app == undefined) {
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The function must be called from an App Check verified app.");
+      }
 
-exports.addCustomClaims = functions.https.onCall((data, context) => {
-  const {uid, customClaims} = data;
-  if (context.auth.token.admin) {
-    return admin.auth().setCustomUserClaims(uid, customClaims)
-        .then(() => {
-          return listUsers();
-        })
-        .catch((error) => {
-          return {error: error.message, customClaims: customClaims, uid: uid};
-        });
-  } else {
-    throw new Error("You don't have admin permission");
-  }
-});
+      if (context.auth.token.admin) {
+        return listUsers();
+      } else {
+        throw new functions.https.HttpsError("permission-denied",
+            "You don't have admin permission");
+      }
+    });
+
+exports.addCustomClaims = functions.runWith({enforceAppCheck: true})
+    .https.onCall((data, context) => {
+      if (context.app == undefined) {
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The function must be called from an App Check verified app.");
+      }
+
+      const {uid, customClaims} = data;
+      if (context.auth.token.admin) {
+        return admin.auth().setCustomUserClaims(uid, customClaims)
+            .then(() => {
+              return listUsers();
+            })
+            .catch((error) => {
+              return {
+                error: error.message, customClaims: customClaims, uid: uid,
+              };
+            });
+      } else {
+        throw new Error("You don't have admin permission");
+      }
+    });
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +18,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+// eslint-disable-next-line no-restricted-globals
+self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6Lf4Lr8lAAAAAFRP1ERgboJ32VJrWstLNyHOp3gg'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true,
+});
 export default app;
